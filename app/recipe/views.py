1c07b8047ac7016039c5911b192eb480392f2dad
Moise1 <rwibutso.mo@gmail.com>
2024-11-05 15:05:12 +0200

    implement tags API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -22,7 +22,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
     Recipe,
-    # Tag,
+    Tag,
     # Ingredient,
 )
 from recipe import serializers
@@ -132,10 +132,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
 #         ).order_by('-name').distinct()
 
 
-# class TagViewSet(BaseRecipeAttrViewSet):
-#     """Manage tags in the database."""
-#     serializer_class = serializers.TagSerializer
-#     queryset = Tag.objects.all()
+class TagViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+    ):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """ Returns tags belonging to the authenticated userr """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
 # class IngredientViewSet(BaseRecipeAttrViewSet):
